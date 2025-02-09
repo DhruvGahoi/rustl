485214312f65369f430aedb7de20d1ceb8f548f0
DhruvGahoi <dhruvgahoi11111@gmail.com>
2025-02-09 19:40:08 +0530

    references and borrowing

--- ch-04/src/main.rs
+++ ch-04/src/main.rs
@@ -59,7 +59,23 @@ fn main() {
     let (str, len) = calculate_len(str);
     println!("The len of {str} is {len}");
     // This fn creates an internal variable and gives its ownership to us
+
+    // ---------------- REFERENCE AND BORROWING ------------------
+    let mut strr = String::from("Hello");
+    let lent = calculate_length(&mut strr);
+    println!("Length of {strr} is {lent}");
+
+    // Mutable references have one big restriction:
+    let mut strrr = String::from("whwiwfds");
+    let str1 = &mut strrr;
+    // if you have a mutable reference to a value, you can have no other references to that value.
+    // let str2 = &strrr;
+    // let str3 = &strrr;
+
+    str1.push_str(" wiuhiuwhi");
+    println!("s1 {s1}");
 }
+
 fn gives_ownership() -> String {
     let s = String::from("This is a string from takes ownership fn");
     s // we r returning "s"
@@ -83,3 +99,17 @@ fn calculate_len(s: String) -> (String, usize) {
     let res = s.len();
     (s, res)
 }
+
+// REFERENCE AND BORROWING
+// What we are doing here is, passing the reference of that object so it doesnt need to have the
+// ownership of that, and instead of returning the tuple, we can simply return the len
+fn calculate_length(s: &mut String) -> usize {
+    //                 ^^^^ we can also give the mutable reference so that we have the perms to
+    //                 mutate this String
+    //But we also need to pass the &mut at the top, so we have the mut reference
+    // // s is a reference to a String
+    s.push_str("Hello there");
+    let res = s.len();
+    res
+} // // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, it is not dropped.
